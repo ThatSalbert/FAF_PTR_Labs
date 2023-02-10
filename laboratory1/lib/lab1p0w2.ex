7595e0cf813e051e63f6e62aed0eebf7f31169ed
Salber <numeoriginaldemail@gmail.com>
2023-02-10 15:41:17 +0200

    Final Changes to Week 1 and 2 of Lab1

---
+++ laboratory1/lib/lab1p0w2.ex
@@ -0,0 +1,174 @@
+defmodule Lab1P0W2 do
+  def isPrime(2), do: true
+  def isPrime(n) do
+    result = Enum.any?(2..n-1, fn i -> rem(n, i) == 0 end)
+    !result
+  end
+
+  def cylinderArea(r, h) do
+    Float.ceil(2*:math.pi()*r*(r + h), 4)
+  end
+
+  def reverse(list) do
+    Enum.reverse(list)
+  end
+
+  def uniqueSum(list) do
+    Enum.uniq(list) |> Enum.sum()
+  end
+
+  def extractRandomN(list, n) do
+    Enum.take_random(list, n)
+  end
+
+  def firstFibonacciElements(1), do: [1]
+  def firstFibonacciElements(2), do: [1, 1]
+  def firstFibonacciElements(n) do
+    previous = firstFibonacciElements(n-1)
+    Enum.map(previous, fn x -> x end) ++ [Enum.at(previous, -1) + Enum.at(previous, -2)]
+  end
+
+  def translator(dictionary, original_string) do
+    String.replace(original_string, ~r/\b\w+\b/, fn word ->
+      Map.get(dictionary, String.to_atom(word), word)
+    end)
+  end
+
+  def smallestNumber(0, 0, 0), do: [0, 0, 0] |> Enum.join()
+  def smallestNumber(n1, n2, n3) do
+    list = [n1, n2, n3] |> Enum.sort()
+    if Enum.at(list, 0) == 0 do
+      if Enum.at(list, 1) == 0 do
+          [Enum.at(list, 2), Enum.at(list, 1), Enum.at(list, 0)] |> Enum.join()
+      else
+          [Enum.at(list, 1), Enum.at(list, 0), Enum.at(list, 2)] |> Enum.join()
+      end
+    else
+      [Enum.at(list, 0), Enum.at(list, 1), Enum.at(list, 2)] |> Enum.join()
+    end
+  end
+
+  def rotateLeft(list, n) do
+    {left, right} = Enum.split(list, n)
+    right ++ left
+  end
+
+  def listRightAngleTriangles() do
+    Enum.reduce(1..20, [], fn a, acc ->
+      Enum.reduce(1..20, acc, fn b, acc ->
+        Enum.reduce(1..28, acc, fn c, acc ->
+          if :math.pow(a, 2) + :math.pow(b, 2) == :math.pow(c, 2) do
+            acc ++ [[a, b, c]]
+          else
+            acc
+          end
+        end)
+      end)
+    end)
+  end
+
+  def removeConsecutiveDuplicates(list) do
+    Enum.reduce(list, [], fn n, acc ->
+      if Enum.at(acc, -1) == n do
+        acc
+      else
+        acc ++ [n]
+      end
+    end)
+  end
+
+  def lineWords(list) do
+    line1 = "qwertyuiopQWERTYUIOP"
+    line2 = "asdfghjklASDFGHJKL"
+    line3 = "zxcvbnmZXCVBNM"
+    lines = [String.graphemes(line1), String.graphemes(line2), String.graphemes(line3)]
+
+    Enum.reduce(list, [], fn word, acc ->
+      Enum.reduce(lines, acc, fn line, acc ->
+        if Enum.all?(String.graphemes(word), fn char -> Enum.member?(line, char) end) do
+          acc ++ [word]
+        else
+          acc
+        end
+      end)
+    end)
+  end
+
+  def encode(word, n) do
+    alphabet = String.graphemes("abcdefghijklmnopqrstuvwxyz")
+    word = String.graphemes(String.downcase(word))
+    Enum.reduce(word, [], fn char, acc ->
+      if Enum.member?(alphabet, char) do
+        index = Enum.find_index(alphabet, fn y -> y == char end)
+        acc ++ [Enum.at(alphabet, rem(index + n, 26))]
+      else
+        acc ++ [char]
+      end
+    end) |> Enum.join()
+  end
+
+  def decode(word, n) do
+    alphabet = String.graphemes("abcdefghijklmnopqrstuvwxyz")
+    word = String.graphemes(String.downcase(word))
+    Enum.reduce(word, [], fn char, acc ->
+      if Enum.member?(alphabet, char) do
+        index = Enum.find_index(alphabet, fn y -> y == char end)
+        acc ++ [Enum.at(alphabet, rem(index - n, 26))]
+      else
+        acc ++ [char]
+      end
+    end) |> Enum.join()
+  end
+
+  def lettersCombinations(string) do
+    numpad = %{
+      "2" => ['a', 'b', 'c'],
+      "3" => ['d', 'e', 'f'],
+      "4" => ['g', 'h', 'i'],
+      "5" => ['j', 'k', 'l'],
+      "6" => ['m', 'n', 'o'],
+      "7" => ['p', 'q', 'r', 's'],
+      "8" => ['t', 'u', 'v'],
+      "9" => ['w', 'x', 'y', 'z']
+    }
+
+    Enum.reduce(String.graphemes(string), [], fn char, acc ->
+      if acc == [] do
+        numpad[char]
+      else
+        cartesianProduct(acc, numpad[char])
+      end
+    end)
+  end
+
+  def cartesianProduct(list1, list2) do
+    for x <- list1, y <- list2, do: x ++ y
+  end
+
+  def groupAnagrams(list) do
+    Enum.group_by(list, fn word -> word |> String.graphemes() |> Enum.sort() |> Enum.join() end)
+  end
+
+  @romanMap [
+    {1000, "M"},
+    {900, "CM"},
+    {500, "D"},
+    {400, "CD"},
+    {100, "C"},
+    {90, "XC"},
+    {50, "L"},
+    {40, "XL"},
+    {10, "X"},
+    {9, "IX"},
+    {5, "V"},
+    {4, "IV"},
+    {1, "I"}
+  ]
+
+  def toRoman(0), do: ""
+  def toRoman(n) do
+    Enum.reduce(@romanMap, {"", n}, fn {value, roman}, {acc, n} ->
+      {acc <> String.duplicate(roman, div(n, value)), rem(n, value)}
+    end) |> elem(0)
+  end
+end
--- laboratory1/test/laboratory1_test.exs
+++ laboratory1/test/laboratory1_test.exs
@@ -4,80 +4,80 @@ defmodule Laboratory1Test do
   import ExUnit.CaptureIO
 
   test "Prints: Hello PTR" do
-    lab1_res = capture_io(fn -> Laboratory1.print_ptr() end)
+    lab1_res = capture_io(fn -> Lab1P0W1.print_ptr() end)
     assert lab1_res == "Hello PTR\n"
   end
 
   test "Checks if the given number is prime" do
-    result = Laboratory1.isPrime(2)
+    result = Lab1P0W2.isPrime(2)
     assert result == true
 
-    result = Laboratory1.isPrime(3)
+    result = Lab1P0W2.isPrime(3)
     assert result == true
 
-    result = Laboratory1.isPrime(281)
+    result = Lab1P0W2.isPrime(281)
     assert result == true
 
-    result = Laboratory1.isPrime(923)
+    result = Lab1P0W2.isPrime(923)
     assert result == false
   end
 
   test "Calculates the total area of a cylinder" do
-    result = Laboratory1.cylinderArea(4, 3)
+    result = Lab1P0W2.cylinderArea(4, 3)
     assert result == 175.9292
 
-    result = Laboratory1.cylinderArea(11, 7)
+    result = Lab1P0W2.cylinderArea(11, 7)
     assert result == 1244.0707
 
-    result = Laboratory1.cylinderArea(19, 3)
+    result = Lab1P0W2.cylinderArea(19, 3)
     assert result == 2626.3715
   end
 
   test "Reverses given list" do
-    result = Laboratory1.reverse([1, 2, 3, 4, 5])
+    result = Lab1P0W2.reverse([1, 2, 3, 4, 5])
     assert result == [5, 4, 3, 2, 1]
 
-    result = Laboratory1.reverse([19, -1, 9, 2, 0])
+    result = Lab1P0W2.reverse([19, -1, 9, 2, 0])
     assert result == [0, 2, 9, -1, 19]
   end
 
   test "Calculates the sum of unique elements in a list" do
-    result = Laboratory1.uniqueSum([1, 2, 3, 4, 5])
+    result = Lab1P0W2.uniqueSum([1, 2, 3, 4, 5])
     assert result == 15
 
-    result = Laboratory1.uniqueSum([1, 2, 2, 3, 4, 5, 5, 1, 2, 3, 4, 5])
+    result = Lab1P0W2.uniqueSum([1, 2, 2, 3, 4, 5, 5, 1, 2, 3, 4, 5])
     assert result == 15
 
-    result = Laboratory1.uniqueSum([1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 6, 8, 8, 10, 12, 11, 20])
+    result = Lab1P0W2.uniqueSum([1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 6, 8, 8, 10, 12, 11, 20])
     assert result == 82
   end
 
   test "Returns the first n elements of the Fibonacci sequence" do
-    result = Laboratory1.firstFibonacciElements(1)
+    result = Lab1P0W2.firstFibonacciElements(1)
     assert result == [1]
 
-    result = Laboratory1.firstFibonacciElements(2)
+    result = Lab1P0W2.firstFibonacciElements(2)
     assert result == [1, 1]
 
-    result = Laboratory1.firstFibonacciElements(3)
+    result = Lab1P0W2.firstFibonacciElements(3)
     assert result == [1, 1, 2]
 
-    result = Laboratory1.firstFibonacciElements(4)
+    result = Lab1P0W2.firstFibonacciElements(4)
     assert result == [1, 1, 2, 3]
 
-    result = Laboratory1.firstFibonacciElements(5)
+    result = Lab1P0W2.firstFibonacciElements(5)
     assert result == [1, 1, 2, 3, 5]
 
-    result = Laboratory1.firstFibonacciElements(6)
+    result = Lab1P0W2.firstFibonacciElements(6)
     assert result == [1, 1, 2, 3, 5, 8]
 
-    result = Laboratory1.firstFibonacciElements(7)
+    result = Lab1P0W2.firstFibonacciElements(7)
     assert result == [1, 1, 2, 3, 5, 8, 13]
 
-    result = Laboratory1.firstFibonacciElements(8)
+    result = Lab1P0W2.firstFibonacciElements(8)
     assert result == [1, 1, 2, 3, 5, 8, 13, 21]
 
-    result = Laboratory1.firstFibonacciElements(9)
+    result = Lab1P0W2.firstFibonacciElements(9)
     assert result == [1, 1, 2, 3, 5, 8, 13, 21, 34]
   end
 
@@ -87,40 +87,40 @@ defmodule Laboratory1Test do
       papa: "father"
     }
     original_string = "mama is with papa"
-    result = Laboratory1.translator(dictionary, original_string)
+    result = Lab1P0W2.translator(dictionary, original_string)
     assert result == "mother is with father"
   end
 
   test "Returns smallest number from 3 given digits" do
-    result = Laboratory1.smallestNumber(0, 0, 0)
+    result = Lab1P0W2.smallestNumber(0, 0, 0)
     assert result == "000"
 
-    result = Laboratory1.smallestNumber(0, 5, 1)
+    result = Lab1P0W2.smallestNumber(0, 5, 1)
     assert result == "105"
 
-    result = Laboratory1.smallestNumber(5, 1, 5)
+    result = Lab1P0W2.smallestNumber(5, 1, 5)
     assert result == "155"
 
-    result = Laboratory1.smallestNumber(7, 0, 0)
+    result = Lab1P0W2.smallestNumber(7, 0, 0)
     assert result == "700"
 
-    result = Laboratory1.smallestNumber(1, 2, 3)
+    result = Lab1P0W2.smallestNumber(1, 2, 3)
     assert result == "123"
   end
 
   test "Rotate list n places to the left" do
-    result = Laboratory1.rotateLeft([1, 2, 3, 4, 5], 2)
+    result = Lab1P0W2.rotateLeft([1, 2, 3, 4, 5], 2)
     assert result == [3, 4, 5, 1, 2]
 
-    result = Laboratory1.rotateLeft([9, 1, 5, 9, 5, 8], 3)
+    result = Lab1P0W2.rotateLeft([9, 1, 5, 9, 5, 8], 3)
     assert result == [9, 5, 8, 9, 1, 5]
   end
 
   test "Eliminate conseuctive duplicates in a given list" do
-    result = Laboratory1.removeConsecutiveDuplicates([1, 2, 2, 3, 2, 4, 4, 3, 4, 5, 5, 4, 5, 5])
+    result = Lab1P0W2.removeConsecutiveDuplicates([1, 2, 2, 3, 2, 4, 4, 3, 4, 5, 5, 4, 5, 5])
     assert result == [1, 2, 3, 2, 4, 3, 4, 5, 4, 5]
 
-    result = Laboratory1.removeConsecutiveDuplicates([1, 2, 2, 2, 4, 8, 4])
+    result = Lab1P0W2.removeConsecutiveDuplicates([1, 2, 2, 2, 4, 8, 4])
     assert result == [1, 2, 4, 8, 4]
   end
 end
