ab439f75064e41c9600fa41b89255f1f2c4988ad
Salbert <numeoriginaldemail@gmail.com>
2023-04-12 10:32:20 +0300

    Laboratory 2 - FINAL changes to Batcher.

--- laboratory2/lib/batcher.ex
+++ laboratory2/lib/batcher.ex
@@ -2,71 +2,39 @@ defmodule Batcher do
   use GenServer
 
   def start_link(batch_size, time_to_wait) do
-    ticks = time_to_wait / 1000 |> trunc
-    GenServer.start_link(__MODULE__, {batch_size, time_to_wait, [], ticks}, name: __MODULE__)
+    GenServer.start_link(__MODULE__, {batch_size, time_to_wait, [], System.system_time(:millisecond)}, name: __MODULE__)
   end
 
-  def init({batch_size, time_to_wait, messagesToPrintMap, ticks}) do
-    Process.send_after(self(), {:timerTick, time_to_wait}, 0)
-    {:ok, {batch_size, time_to_wait, messagesToPrintMap, ticks}}
+  def init({batch_size, time_to_wait, messagesToPrintMap, currentTick}) do
+    {:ok, {batch_size, time_to_wait, messagesToPrintMap, currentTick}}
   end
-
-  def handle_info(:printMessagesIfNotFull, {batch_size, time_to_wait, messagesToPrintMap, ticks}) do
-    cond do
-      Enum.count(messagesToPrintMap) != batch_size ->
-        IO.inspect("Printing because time is up")
-        IO.puts("")
-        Enum.reduce(messagesToPrintMap, fn tweet, _ ->
-          IO.inspect tweet
-        end)
-        newMessagesToPrintMap = List.delete(messagesToPrintMap, messagesToPrintMap)
-        ticksCalc = time_to_wait / 1000 |> trunc
-        send(self(), {:setTicks, ticksCalc})
-        {:noreply, {batch_size, time_to_wait, newMessagesToPrintMap, ticks}}
-      length(messagesToPrintMap) == batch_size ->
-        {:noreply, {batch_size, time_to_wait, messagesToPrintMap, ticks}}
-    end
-  end
-
-  def handle_info({:tweet, {id, tweet, sentiment, engagement}}, {batch_size, time_to_wait, messagesToPrintMap, ticks}) do
+  
+  def handle_info({:tweet, {id, tweet, sentiment, engagement}}, {batch_size, time_to_wait, messagesToPrintMap, currentTick}) do
+    tick = currentTick
+    tickNow = System.system_time(:millisecond)
+    tickDiff = tickNow - tick
     messagesToPrint = "Tweet: #{tweet} | Sentiment: #{sentiment} | Engagement: #{engagement} | ID: #{id}"
     newMessagesToPrintMap = messagesToPrintMap ++ [messagesToPrint]
-    currentMap = if length(newMessagesToPrintMap) == batch_size do
-      IO.inspect("Printing because batch size")
-      IO.puts("")
-      Enum.reduce(newMessagesToPrintMap, fn tweet, _ ->
-        IO.inspect tweet
-      end)
-      newMessagesToPrintMap = []
-      ticksCalc = time_to_wait / 1000 |> trunc
-      send(self(), {:setTicks, ticksCalc})
-      newMessagesToPrintMap
-    else
-      newMessagesToPrintMap
+    {currentMap, ticks} = cond do
+      length(newMessagesToPrintMap) == batch_size and tickDiff < time_to_wait ->
+        IO.puts("Printing because full")
+        IO.puts("")
+        Enum.reduce(newMessagesToPrintMap, fn tweet, _ ->
+          IO.inspect tweet
+        end)
+        ticksCalc = System.system_time(:millisecond)
+        {[], ticksCalc}
+      tickDiff >= time_to_wait ->
+        IO.puts("Printing because timer")
+        IO.puts("")
+        Enum.reduce(newMessagesToPrintMap, fn tweet, _ ->
+          IO.inspect tweet
+        end)
+        ticksCalc = System.system_time(:millisecond)
+        {[], ticksCalc}
+      true ->
+        {newMessagesToPrintMap, tick}
     end
     {:noreply, {batch_size, time_to_wait, currentMap, ticks}}
-  end
-
-  def handle_info({:setTicks, setTheTicks}, state) do
-    send(self(), {:timerTick, setTheTicks})
-    {:noreply, state}
-  end
-
-  def handle_info({:timer, currentTick}, state) do
-    newTick = currentTick - 1
-    if newTick == 0 do
-      send(self(), :printMessagesIfNotFull)
-      {:noreply, state}
-    else
-      {:noreply, state}
-    end
-  end
-
-  def handle_info({:timerTick, newTick}, state) do
-    newTick = newTick - 1
-    Process.sleep(1000)
-    send(self(), {:timerTick, newTick})
-    send(self(), {:timer, newTick})
-    {:noreply, state}
   end
 end
--- laboratory2/lib/laboratory2.ex
+++ laboratory2/lib/laboratory2.ex
@@ -4,8 +4,8 @@ defmodule Laboratory2 do
   def start(_type, _args) do
     min_time = 100
     max_time = 500
-    batch_size = 25
-    batch_time = 10000
+    batch_size = 2500
+    batch_time = 5000
     {:ok, _} = WorkerPoolSupervisor.start_link(min_time, max_time, batch_size, batch_time)
     {:ok, _} = GenericLoadBalancer.start_link()
     # {:ok, _} = Manager.start_link(5000, 7, 1)
