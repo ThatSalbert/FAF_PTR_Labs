463ff3ce92f9aa821913d6e405b92055b9578f66
Salber <numeoriginaldemail@gmail.com>
2023-05-16 13:08:16 +0300

    Laboratory 3 - Small changes

--- laboratory3/lib/messagebroker.ex
+++ laboratory3/lib/messagebroker.ex
@@ -1,12 +1,5 @@
 defmodule MessageBroker do
   use GenServer
-  use AMQP
 
-  def start_link do
-    GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
-  end
-
-  def init(:ok) do
-    {:ok, }
-  end
+  
 end
--- laboratory3/mix.exs
+++ laboratory3/mix.exs
@@ -24,7 +24,7 @@ defmodule Laboratory3.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:amqp, "~> 3.2.0"},
+      {:jason, "~> 1.4.0"},
     ]
   end
 end
